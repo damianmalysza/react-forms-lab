983aa3d88dff28d64dfeb3cedfd1cde0a562a81a
Damian Malysza <damian.malysza@gmail.com>
2021-12-29 13:28:37 -0500

    commit tree 920cb3e953b007864ab4922add3a25bf7f4ca946

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,24 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      tweet: ''
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      tweet: event.target.value
+    })
   }
 
   render() {
+    let charsLeft = this.props.maxChars - this.state.tweet.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.tweet} onChange={this.handleChange}/>
+        <p>Chars Left: {charsLeft}</p>
       </div>
     );
   }
